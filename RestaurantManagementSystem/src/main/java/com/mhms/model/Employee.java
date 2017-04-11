303441226c958c3aa96eef0ac5b41d5dafa38a53
manoj chaudhary <ctmanoj55@gmail.com>
2017-04-10 23:50:43 -0500

    add Menu class

---
+++ RestaurantManagementSystem/src/main/java/com/mhms/model/Employee.java
@@ -0,0 +1,24 @@
+package com.mhms.model;
+
+
+
+public abstract class Employee {
+    private Integer empId;
+    private String name;
+
+    public Integer getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(Integer empId) {
+        this.empId = empId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
